b43ef5f6dbd24d21b388611e90931cbfdadbc6ba
Vee Satayamas <vsatayamas@gmail.com>
2024-04-27 11:54:10 +0700

    use decode in embedding

--- embedding.lisp
+++ embedding.lisp
@@ -1,27 +1,34 @@
 (in-package :llama)
 
-(defun %embedding (ctx tokens txt verbose add-beginning-of-sentence threads)
+(defun %embedding (ctx tokens txt verbose add-beginning-of-sentence)
   (tokenize (model ctx) tokens txt :add-beginning-of-sentence add-beginning-of-sentence)
-  (when (> verbose 0) (print (list-tokens tokens :context ctx :limit nil)))  
-  (evaluate ctx tokens 0 threads) ;; n-past = 0
+  (when (> verbose 0) (print (list-tokens tokens :context ctx :limit nil)))
+  (let ((batch (get-one-batch tokens (n tokens) 0 0)))
+    (decode ctx batch))
+
+  ;;  (evaluate ctx tokens 0 threads) ;; n-past = 0
   (get-embeddings ctx))
 
 (defun embedding (prompt &key (model *model*) (n-ctx *n-ctx*) (ntokens n-ctx) (verbose 0) ;; (numa *numa*)
-			   (add-beginning-of-sentence t) (threads *threads*) (ngl *ngl*))
+			   (add-beginning-of-sentence t) (threads *threads*) (threads-batch *threads-batch*)
+			   (ngl *ngl*))
   "Calculate embeddings for the given prompt. If passed a list of prompts it will loop over them."
   #+sbcl (sb-ext::set-floating-point-modes :traps nil)
   (llama-backend-init)
   (let* ((mdl (make-instance 'mdl :file model
 				  :params (model-parameters :n-gpu-layers ngl)))
 	 (ctx (make-instance 'ctx :model mdl
-				  :params (context-parameters :embedding t :n-ctx n-ctx)))
+				  :params (context-parameters :embedding t
+							      :n-ctx n-ctx
+							      :n-threads threads
+							      :n-threads-batch threads-batch)))
 	 (tokens (make-instance 'tokens :size ntokens)))
     (prog1
 	(if (listp prompt)
 	    (loop for text in prompt
 		  do (format t ".")
-		  collect (%embedding ctx tokens text verbose add-beginning-of-sentence threads))
-	    (%embedding ctx tokens prompt verbose add-beginning-of-sentence threads))
+		  collect (%embedding ctx tokens text verbose add-beginning-of-sentence))
+	    (%embedding ctx tokens prompt verbose add-beginning-of-sentence))
       (when (> verbose 1) (print-timings ctx)))))
 
 ;; ./embedding -p "testing" -ngl 1 | head -c 28
